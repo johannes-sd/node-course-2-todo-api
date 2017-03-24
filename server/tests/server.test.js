acae4c64ba1b8d2d6ab043edf9759cccdbab31ff
Johan Martin Johannessen <johannes@sd.no>
2017-03-24 05:56:45 +0100

    Siste testsak fungerer ikke, får ikke ut rette objekt

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,14 @@
 const expect = require("expect");
 const request = require("supertest");
+const {ObjectId} = require("mongodb");
 
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
 
 const todos = [
-    {text: "first test todo"},
-    {text: "second test todo"}
+    {_id: new ObjectId, text: "first test todo"},
+    {_id: new ObjectId, text: "second test todo"}
 ]
 // testing lifesycle method
 beforeEach((done) =>{
@@ -65,5 +66,18 @@ describe('GET /todos', () => {
                 expect(res.body.todos.length).toBe(2);
             })
             .end(done);
+    });
+});
+
+describe('GET /todos/:id', ()=>{
+    console.log(`${todos[0]._id.toHexString()}`);   
+    it('should return doc', (done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res)=>{
+                console.log("res " + res.body.Todo);
+                expect(res.body.todos.text).toBe(todos[0].text);
+            }).end(done);
     });
 });
