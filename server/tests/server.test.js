75102ac91ad48d10912f4bac0411954c2e4d5047
Johan Martin Johannessen <johannes@sd.no>
2017-03-30 06:21:16 +0200

    TEST the delete todos by id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,4 +96,39 @@ describe('GET /todos/:id', () => {
         .end(done);
     });
 });
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexID = todos[1]._id.toHexString();
+        request(app)
+            .delete('/todos/' + hexID)
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.todo._id).toBe(hexID);
+                })
+                .end((err, res) => {    
+                    if(err){
+                        return done(err);
+                    }
+                    Todo.findById(hexID).then((todo) => {
+                        expect(todo).toNotExist();
+                        done();
+                    }).catch((e) => done(e));
+                    // query database using findById toNotExist
+                    // expect(null).toNotExist()
+                });
+    });
+    it('should return a 404 if todo not found', (done) =>{
+        var fakeID = new ObjectId().toHexString();
+        request(app)
+        .delete('/todos/' + fakeID)
+        .expect(404)
+        .end(done);
+    });
+    it('should return a 404 if ObjectId is invalid', (done) => {
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done);
+    });
+});
 
