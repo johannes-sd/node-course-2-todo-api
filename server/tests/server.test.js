fbaa6f5a56886dca1d8f07063486e4ed1f142015
Johan Martin Johannessen <johannes@sd.no>
2017-05-31 03:41:57 +0200

    Add test for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -215,7 +215,7 @@ describe('POST /users', () => {
                 expect(user).toExist();
                 expect(user.password).toNotBe(password);
                 done();
-            });
+            }).catch((e) => done(e));
         });
     });
     it('should return validation errors if request invalid', (done) => {
@@ -240,4 +240,53 @@ describe('POST /users', () => {
             expect(res.body) == {};
         }).end(done);
     });
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return token', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: users[1].password
+        })
+        .expect(200)
+        .expect((res)=>{
+            expect(res.headers['x-auth']).toExist();
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens[0]).toInclude({
+                    access: 'auth',
+                    token: res.headers['x-auth']
+                });
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+    it('sould reject invalid login', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: 'an invalid password'
+        })
+        .expect(400)
+        .expect((res)=>{
+            expect(res.headers['x-auth']).toNotExist();
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens[0]).toEqual(null);
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+    
 });
